f7c33f0c79882c9903e2a767e65ccf0b438f3f97
Hutsh <hxbbun@gmail.com>
2016-04-04 12:02:02 +0800

    excerse 10

--- ex/ex10.py
+++ ex/ex10.py
@@ -1,1 +1,23 @@
-ex10.py
+print "\\,\',\",\a,\b,\f,\n"
+
+print "I am 6'2\" tall."
+print 'I am 6\'2" tall.'
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backlash_cat = "I'm \\ a \\ cat."
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishes
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backlash_cat
+print fat_cat
+
+while True:
+    for i in ["/","-","|","\\","|"]:
+        print "%s\r" % i,
